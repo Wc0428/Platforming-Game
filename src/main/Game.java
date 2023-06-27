6667b150a9fa2c1e3ba8de8330a7e66302251282
Wc0428 <109028745+Wc0428@users.noreply.github.com>
2023-06-28 00:24:51 +0800

    create JPanel class

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Game {
-
+	
+	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+	
+	public Game() {
+		gamePanel = new GamePanel();
+		gameWindow = new GameWindow(gamePanel);
+		
+	}
 }
